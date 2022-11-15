72e45a70d4fc622361f2c0d6da738100b75d440b
Abhishek Kumar <akroy2456@gmail.com>
2022-11-15 17:08:21 +0530

    updated files for deploy

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const connectMongo=require('./db');
 const cors=require('cors');
+require('dotenv').config();
 app.use(cors());
 
 connectMongo();
